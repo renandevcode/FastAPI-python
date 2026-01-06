2b885930f11f062bf3f48a3d00c6582363732119
renan <renanzin007@icloud.com>
2026-01-06 00:14:15 -0300

    corrigindo método delete

--- app/main.py
+++ app/main.py
@@ -23,12 +23,14 @@ def read_user(user_id:int,db:Session=Depends(get_db)):
         raise HTTPException(status_code=400,detail="Usuário não encontrado")
     return db_user
 
-@app.delete("/users/{user_id}")
-def delete_user(user_id:int,db:Session=Depends(get_db)):
-    db_user=crud.get_user(db,user_id=user_id)
-    if db_user is None:
-        raise HTTPException(status_code=400,detail="Usuário não encontrado")
-    crud.delete_user(db,user_id=user_id)
-    return {"message":"Usuário deletado com sucesso"}
+@app.delete("/users/{user_id}", status_code=204)
+def delete_user(user_id: int, db: Session = Depends(get_db)):
+    db_user = crud.get_user(db, user_id=user_id)
+
+    if not db_user:
+        raise HTTPException(status_code=404, detail="Usuário não encontrado")
+
+    db.delete(db_user)
+    db.commit()
 
     
